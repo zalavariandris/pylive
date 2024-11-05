a98201fcac0efe5d8ee8bea6c351323466b3f26e
zalavariandris <zalavariandris@gmail.com>
2024-11-05 11:40:36 +0100

    get current AppWidget with a classmethod

--- pylive/LiveScript.py
+++ pylive/LiveScript.py
@@ -16,24 +16,19 @@ import sys
 # def display(msg:Any):
 # 	if preview_widget := cast(QLabel, getWidgetByName("PREVIEW_WINDOW_ID")):
 # 		preview_widget.setText(f"{msg}")
-from textwrap import dedent
-initial_script = dedent("""\
-	from pylive.utils import getWidgetByName
-	import numpy as np
-	def random_image(width=8, height=8):
-		img = np.random.randint(0, 255, (height, width, 3), dtype=np.uint8)
-		# Convert to QImage
-		return QImage(img.data, width, height, 3 * width, QImage.Format_RGB888)
 
-	app_widget = getWidgetByName("PREVIEW_WINDOW_ID")
-	app_widget.display( random_image(128, 128) )
-	mylabel = QLabel()
-	mylabel.setText("hello")
-	app_widget.display(mylabel)
-	print("hello")
-""")
+from pylive.utils import getWidgetByName
+from pylive.unique import make_unique_id
+
+# A_GLOBAL_VAR = 4
 
 class AppWidget(QWidget):
+	_stack = []
+	# def __new__(cls):
+	# 	if not cls._instance:
+	# 		cls._instance = super(AppWidget, cls).__new__(cls)
+	# 	return cls._instance
+
 	def __init__(self, parent: Optional[QWidget]=None) -> None:
 		super().__init__(parent=parent)
 		self.setObjectName("PREVIEW_WINDOW_ID")
@@ -60,6 +55,10 @@ class AppWidget(QWidget):
 		self.layout().addWidget(self.previewScrollArea, 1)
 		self.layout().addWidget(self.loggingScrollArea, 0)
 
+	@classmethod
+	def current(cls):
+		return getWidgetByName(cls._stack[-1])
+
 	def display(self, data:Any):
 		match data:
 			case str():
@@ -79,14 +78,18 @@ class AppWidget(QWidget):
 			case _:
 				self.previewFrame.layout().addWidget(QLabel(str(data)))
 
-
 	def clear(self):
 		layout = self.previewFrame.layout()
 		for i in reversed(range(layout.count())): 
 			layout.itemAt(i).widget().deleteLater()
 
+	def createContext(self):
+		return {'__builtins__': globals()["__builtins__"]}
+
 	def evaluate(self, source:str):
 		self.clear()
+		
+		global_vars = self.createContext()
 
 		old_stdout = sys.stdout # route standard output
 		sys.stdout = mystdout = StringIO()
@@ -94,9 +97,13 @@ class AppWidget(QWidget):
 		# clear output
 		print("\033c") # clear text
 		try:
+			global A_GLOBAL_VAR
+			A_GLOBAL_VAR = "HELLO EXEC CONTEXT"
+			self._stack.append(self.objectName())
 			start_time = time.perf_counter()
+			
 			# compiled = compile(source, "<string>", mode="exec")
-			exec(source, globals(), globals())
+			exec(source, global_vars)
 			end_time = time.perf_counter()
 			duration_ms = (end_time - start_time) * 1000
 
@@ -105,7 +112,10 @@ class AppWidget(QWidget):
 		except Exception as err:
 			print(err)
 		finally:
+			self._stack.pop()
 			sys.stdout = old_stdout # restore standard output
+
+
 
 		# display message on the logging label
 		message = mystdout.getvalue()
@@ -142,7 +152,7 @@ class LiveScript(QWidget):
 		self.splitter.addWidget(self.app_widget)
 		self.splitter.setSizes([self.width()//2,self.width()//2])
 
-		self.setScript(initial_script)
+		self.setScript("print('hello')")
 
 	def setAppWidget(self):
 		pass
@@ -150,11 +160,29 @@ class LiveScript(QWidget):
 	def setScript(self, text:str):
 		self.script_edit.setPlainText(text)
 
+from textwrap import dedent
+initial_script = dedent("""\
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+import numpy as np
+def random_image(width=8, height=8):
+	img = np.random.randint(0, 255, (height, width, 3), dtype=np.uint8)
+	# Convert to QImage
+	return QImage(img.data, width, height, 3 * width, QImage.Format_RGB888)
+
+from pylive.livescript import AppWidget
+app = AppWidget.current()
+
+app.display("HELLO")
+""")
+
 if __name__ == "__main__":
+	import pylive
 	import sys
 	import subprocess
 	app = QApplication(sys.argv)
-	window = LiveScript()
+	window = pylive.livescript.LiveScript()
 
 	window.setScript(initial_script)
 
@@ -163,4 +191,3 @@ if __name__ == "__main__":
 
 	window.show()
 	sys.exit(app.exec())
-
