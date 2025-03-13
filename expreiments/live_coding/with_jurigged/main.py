be5cda701597f04316660c8f007440b9dd931155
zalavariandris <zalavariandris@gmail.com>
2025-03-13 08:18:20 +0100

    save

--- expreiments/live_coding/with_jurigged/main.py
+++ expreiments/live_coding/with_jurigged/main.py
@@ -1,1 +1,44 @@
-import jurigged
+# import jurigged
+import ast
+from unittest import result
+import textual
+
+expr = "[item for item in item]"
+
+def parse_expression(expr:str):
+	return f"parse: {expr}"
+
+
+
+# if __name__ == "__main__":
+# 	import time
+# 	while True:
+# 		time.sleep(1)
+# 		result = parse_expression(expr)
+# 		print(result+"\n")
+
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header, Label
+
+
+class StopwatchApp(App):
+    """A Textual app to manage stopwatches."""
+
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Label(parse_expression(expr))
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
+
+
+if __name__ == "__main__":
+    app = StopwatchApp()
+    app.run()
