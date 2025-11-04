9d658e7b369077cf9e32bc80b371c5d72475a59a
zalavariandris <zalavariandris@gmail.com>
2025-11-04 08:04:47 +0100

    save

--- pylive/camera_spy/demo/ui/viewport2.py
+++ pylive/camera_spy/demo/ui/viewport2.py
@@ -30,12 +30,8 @@ def make_ortho(xmin:float, ymin:float, xmax:float, ymax:float, near:float=-1.0, 
 
     return projection, near, far
 
-def _project(point:Iterable[int|float], projection:glm.mat4, view:glm.mat4)->imgui.ImVec2Like:
+def _project(point:Iterable[int|float], projection:glm.mat4, view:glm.mat4, widget_rect:Tuple[int, int, int, int])->imgui.ImVec2Like:
     assert len(point) == 2 or len(point) == 3
-
-    x, y = imgui.get_window_pos()
-    w, h = imgui.get_window_size()
-    widget_rect = (int(x), int(y), int(w), int(h))
 
     match len(point):
         case 2:
@@ -53,19 +49,6 @@ def _unproject(screen_point:imgui.ImVec2Like, projection:glm.mat4, view:glm.mat4
     widget_rect = (int(x), int(y), int(w), int(h))
     P = glm.unProject(glm.vec3(screen_point.x, screen_point.y, 0), view, projection, widget_rect)
     return imgui.ImVec2(P.x, P.y)
-
-def _get_world_space_mouse_drag_delta(
-        projection:glm.mat4, 
-        view:glm.mat4,
-        button: imgui.MouseButton_=0, 
-        lock_threshold: float=-1
-    )->imgui.ImVec2Like:
-    curr_mouse_pos = imgui.get_mouse_pos()
-    prev_mouse_pos = imgui.get_mouse_pos() - imgui.get_mouse_drag_delta(button, lock_threshold)
-    prev_world = _unproject(prev_mouse_pos, projection, view)
-    curr_world = _unproject(curr_mouse_pos, projection, view)
-    world_space_delta = curr_world - prev_world
-    return world_space_delta
 
 def pan_and_zoom(
         projection:glm.mat4,
@@ -93,10 +76,12 @@ def pan_and_zoom(
 
     # Panning
     if imgui.is_mouse_dragging(imgui.MouseButton_.middle) or (imgui.is_key_down(imgui.Key.left_alt) and imgui.is_mouse_dragging(imgui.MouseButton_.left)):
-        drag_delta = _get_world_space_mouse_drag_delta(projection, view, imgui.MouseButton_.middle if imgui.is_mouse_dragging(imgui.MouseButton_.middle) else imgui.MouseButton_.left)
-        translation.x += drag_delta.x * pan_speed
-        translation.y += drag_delta.y * pan_speed
-        imgui.reset_mouse_drag_delta(imgui.MouseButton_.middle if imgui.is_mouse_dragging(imgui.MouseButton_.middle) else imgui.MouseButton_.left)
+        io = imgui.get_io()
+        prev_world = _unproject(io.mouse_pos_prev, projection, view)
+        curr_world = _unproject(io.mouse_pos, projection, view)
+        world_space_delta = curr_world - prev_world
+        translation.x += world_space_delta.x * pan_speed
+        translation.y += world_space_delta.y * pan_speed
         changed = True
 
     if changed:
@@ -176,20 +161,54 @@ def touch_pad(label:str, size:imgui.ImVec2=imgui.ImVec2(64,64))->Tuple[bool, img
 
 
 from pylive.glrenderer.utils.camera import Camera
-class ViewportState:
-    def __init__(self):
-        self.pan_and_zoom_matrix = glm.identity(glm.mat4)
-        self.camera = Camera().setPosition(glm.vec3(5,5,5)).lookAt(glm.vec3(0,0,0), glm.vec3(0,1,0))
+class Viewport:
+    def __init__(self, content_size:imgui.ImVec2):
+        # canvas size in pixels (aka viewport?)
+        self.content_size = content_size
+
+        # 2D view
+        self.view_2d = glm.identity(glm.mat4)
         self.projection_2d = glm.identity(glm.mat4)
 
+        # 3D view
+        self.use_camera:bool = False
+        self.view_3d = glm.identity(glm.mat4)
+        self.projection_3d = glm.perspective(glm.radians(60.0), float(content_size.x)/float(content_size.y), 0.1, 100.0)
+        
+
+        # updated every frame on begin_viewport
+        self.widget_screen_pos:imgui.ImVec2 = None
+        self.widget_size:imgui.ImVec2 = None
+
+    def project(self, point: glm.vec3) -> glm.vec3:
+        if self.use_camera:
+            return _project(point, 
+                            self.projection_3d, 
+                            self.view_3d,
+                             widget_rect=(self.widget_screen_pos.x, self.widget_screen_pos.y, self.widget_size.x, self.widget_size.y)
+                            )
+        else:
+            return _project(point, 
+                            self.projection_2d, 
+                            self.view_2d,
+                            widget_rect=(self.widget_screen_pos.x, self.widget_screen_pos.y, self.widget_size.x, self.widget_size.y)
+                            )
+
+    def unproject(self, point: glm.vec3) -> glm.vec3:
+        if self.use_camera:
+            return _unproject(point, self.projection_3d, self.view_3d)
+        else:
+            return _unproject(point, self.projection_2d, self.view_2d)
+
 from collections import defaultdict
-viewports = defaultdict(lambda: ViewportState())
+viewports:dict[int|str, Viewport] = dict()
 current_viewport_name:str|None = None
 
-def begin_viewport(name: str, size:imgui.ImVec2=imgui.ImVec2(128,128)):
+def begin_viewport(name: str, content_size: imgui.ImVec2, size:imgui.ImVec2=imgui.ImVec2(128,128)):
     global viewports, current_viewport_name
-    
     current_viewport_name = name
+
+
 
     # widget_screen_pos = imgui.get_window_pos()
     # widget_size = imgui.get_content_region_avail()
@@ -202,36 +221,86 @@ def begin_viewport(name: str, size:imgui.ImVec2=imgui.ImVec2(128,128)):
         imgui.ChildFlags_.borders,
         imgui.WindowFlags_.no_scrollbar
     )
+
+    if name not in viewports:
+        viewports[name] = Viewport(content_size)
+
+    current_viewport = viewports[current_viewport_name]
+    current_viewport.widget_screen_pos = imgui.get_window_pos()
+    current_viewport.widget_size = imgui.get_window_size()
     
     # create 2d projection
-    CONTENT_SIZE = imgui.ImVec2(800,600)
-    projection_2d, near, far = make_ortho(0,0,float(CONTENT_SIZE.x),float(CONTENT_SIZE.y))
-    viewports[current_viewport_name].projection_2d = projection_2d
-    _, viewports[current_viewport_name].pan_and_zoom_matrix = pan_and_zoom(projection_2d, viewports[current_viewport_name].pan_and_zoom_matrix)
-    view_2d = viewports[current_viewport_name].pan_and_zoom_matrix
+    projection_2d, near, far = make_ortho(0,0,float(content_size.x),float(content_size.y))
+    current_viewport.projection_2d = projection_2d
+    
+    imgui.set_cursor_screen_pos(imgui.get_window_pos())
+    imgui.invisible_button("viewport_button", imgui.get_window_size(), imgui.ButtonFlags_.mouse_button_middle)
+    if imgui.is_item_active() or imgui.is_item_hovered():
+        _, current_viewport.view_2d = pan_and_zoom(projection_2d, current_viewport.view_2d)
 
 def end_viewport():
+    global viewports, current_viewport_name
+    current_viewport = viewports[current_viewport_name]
+
+    # draw 2D in content pixelspace
+    widget_rect = (current_viewport.widget_screen_pos.x, current_viewport.widget_screen_pos.y, current_viewport.widget_size.x, current_viewport.widget_size.y)
+    draw_list = imgui.get_window_draw_list()
+    content_screen_tl = _project( (0,0), 
+                                 current_viewport.projection_2d, current_viewport.view_2d, widget_rect)
+    content_screen_br = _project( (current_viewport.content_size.x, current_viewport.content_size.y), 
+                                 current_viewport.projection_2d, current_viewport.view_2d, widget_rect)
+    draw_list.add_rect(content_screen_tl, content_screen_br, colors.YELLOW, thickness=5.0)
     imgui.end_child() # end viewport widget
 
 def guide(A, B):
     global viewports, current_viewport_name
-    projection_2d = viewports[current_viewport_name].projection_2d
-    view_2d = viewports[current_viewport_name].pan_and_zoom_matrix
+    current_viewport = viewports[current_viewport_name]
 
-    A_screen = _project( (A.x, A.z), projection_2d, view_2d)
-    B_screen = _project( (B.x, B.z), projection_2d, view_2d)
+    A_screen = current_viewport.project( (A.x, A.z))
+    B_screen = current_viewport.project( (B.x, B.z))
 
     draw_list = imgui.get_window_draw_list()
     draw_list.add_line(A_screen, B_screen, colors.YELLOW_DIMMED)
 
+def begin_camera(fovy:float, position:glm.vec3, target:glm.vec3, up:glm.vec3=glm.vec3(0,1,0), near=0.1, far=100.0):
+    global viewports, current_viewport_name
+    current_viewport = viewports[current_viewport_name]
+
+    current_viewport.use_camera = True
+    widget_rect = (current_viewport.widget_screen_pos.x, current_viewport.widget_screen_pos.y, current_viewport.widget_size.x, current_viewport.widget_size.y)
+    content_screen_tl = _project( (0,0), 
+                                 current_viewport.projection_2d, current_viewport.view_2d, widget_rect)
+    content_screen_br = _project( (current_viewport.content_size.x, current_viewport.content_size.y), 
+                                 current_viewport.projection_2d, current_viewport.view_2d, widget_rect)
+    projection_3d = make_perspective_projection(
+        fovy,
+        glm.vec2(content_screen_tl.x, content_screen_tl.y),
+        glm.vec2(content_screen_br.x, content_screen_br.y),
+        current_viewport.widget_screen_pos,
+        current_viewport.widget_screen_pos + current_viewport.widget_size,
+        near,
+        far
+    )
+    current_viewport.projection_3d = projection_3d
+    current_viewport.view_3d = glm.lookAt(position, target, up)
+
+def end_camera():
+    global viewports, current_viewport_name
+    current_viewport = viewports[current_viewport_name]
+    current_viewport.use_camera = False
+
 if __name__ == "__main__":
     from imgui_bundle import immapp
+
+    viewports["viewport1"] = Viewport(imgui.ImVec2(800,600))
+    camera = Camera().setPosition(glm.vec3(5,5,5)).lookAt(glm.vec3(0,0,0), glm.vec3(0,1,0))
     
     def gui():
-        global viewports
+        global viewports, current_viewport_name
         imgui.begin("window")
         _, delta = touch_pad("Touch Pad")
-        viewports["viewport1"].camera.orbit(-delta.x * 0.5, -delta.y * 0.5)
+        current_viewport_name = "viewport1"
+        camera.orbit(-delta.x * 0.5, -delta.y * 0.5)
         if _:
             print(f"Touch pad delta: {delta.x}, {delta.y}")
         # begin viewport widget
@@ -252,20 +321,28 @@ if __name__ == "__main__":
         # create 2d projection
         CONTENT_SIZE = imgui.ImVec2(800,600)
         projection_2d, near, far = make_ortho(0,0,float(CONTENT_SIZE.x),float(CONTENT_SIZE.y))
-        _, viewports["viewport1"].pan_and_zoom_matrix = pan_and_zoom(projection_2d, viewports["viewport1"].pan_and_zoom_matrix)
-        view_2d = viewports["viewport1"].pan_and_zoom_matrix
+        _, viewports["viewport1"].view_2d = pan_and_zoom(projection_2d, viewports["viewport1"].view_2d)
+        view_2d = viewports["viewport1"].view_2d
 
         # draw 2D in content pixelspace
         draw_list = imgui.get_window_draw_list()
-        content_screen_tl = _project( (0,0), projection_2d, view_2d)
-        content_screen_br = _project( (CONTENT_SIZE.x, CONTENT_SIZE.y), projection_2d, view_2d)
+        
+        x, y = imgui.get_window_pos()
+        w, h = imgui.get_window_size()
+        widget_rect = (int(x), int(y), int(w), int(h))
+        content_screen_tl = _project( (0,0), projection_2d, view_2d, widget_rect)
+        content_screen_br = _project( (CONTENT_SIZE.x, CONTENT_SIZE.y), projection_2d, view_2d, widget_rect)
         draw_list.add_rect(content_screen_tl, content_screen_br, colors.YELLOW, thickness=5.0)
 
-        # 2d grid        
+        # 2d grid
+        
+        x, y = imgui.get_window_pos()
+        w, h = imgui.get_window_size()
+        widget_rect = (int(x), int(y), int(w), int(h))
         for line in make_grid_lines(step=100, size=512):
             A, B = line
-            A_screen = _project( (A.x, A.z), projection_2d, view_2d)
-            B_screen = _project( (B.x, B.z), projection_2d, view_2d)
+            A_screen = _project( (A.x, A.z), projection_2d, view_2d, widget_rect)
+            B_screen = _project( (B.x, B.z), projection_2d, view_2d, widget_rect)
             draw_list.add_line(A_screen, B_screen, colors.YELLOW_DIMMED)
 
         # create 3d projection
@@ -279,7 +356,7 @@ if __name__ == "__main__":
             far
         )
 
-        view_3d = viewports["viewport1"].camera.viewMatrix()
+        view_3d = camera.viewMatrix()
         # view_3d = glm.lookAt(glm.vec3(5,5,5), glm.vec3(0,0,0), glm.vec3(0,1,0))
 
         # draw 3d grid
@@ -313,13 +390,28 @@ if __name__ == "__main__":
         imgui.end()
 
         imgui.begin("Viewport Demo")
-        begin_viewport("viewport_demo_1", size=imgui.ImVec2(256,256))
+        begin_viewport("viewport_demo_1", content_size=imgui.ImVec2(256,256), size=imgui.ImVec2(256,256))
+        # 2d grid
         for A, B in make_grid_lines(step=20, size=256):
             guide(A, B)
+        begin_camera(math.radians(60.0), glm.vec3(5,5,5), glm.vec3(0,0,1))
+        # 3d grid
+        current_viewport = viewports[current_viewport_name]
+        for A, B in make_grid_lines(step=1, size=10):
+            A_proj = current_viewport.project(A)
+            B_proj = current_viewport.project(B)
+            A_imgui = flipy(A_proj, widget_screen_pos, widget_size)
+            B_imgui = flipy(B_proj, widget_screen_pos, widget_size)
+            draw_list.add_line(A_imgui, B_imgui, colors.YELLOW_DIMMED)
+
+        end_camera()
         end_viewport()
 
-        begin_viewport("viewport_demo_2", size=imgui.ImVec2(256,128))
-
+        begin_viewport("viewport_demo_2", content_size=imgui.ImVec2(256,256), size=imgui.ImVec2(256,128))
+        # 2d grid
+        for A, B in make_grid_lines(step=20, size=256):
+            guide(A, B)
+        
         end_viewport()
         imgui.end()
 
